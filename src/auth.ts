50fd684f028aa1e24e878aadaf9dc2a80d66e3aa
Tyrone Cutajar <tyrone@zumaltd.com>
2021-04-27 17:19:22 +0100

    fixes

--- src/auth.ts
+++ src/auth.ts
@@ -1,24 +1,19 @@
-import { GraphQLClient, gql } from 'graphql-request'
+import { GraphQLClient, gql } from "graphql-request"
 
-const endpoint = "http://localhost/api/graphql"
-const graphQLClient = new GraphQLClient(endpoint, {
-    headers: {
-        "content-type": "application/json",
-    },
-})
 const mutation = gql`
     mutation LoginMutation($username: String!, $password: String!) {
       tokenAuth(username: $username, password: $password) {
         token
         refreshToken
       }
-    } 
+    }
   `
 interface TAuthTokens {
-    AuthTokens: { token: string; refreshToken: string }
+    tokenAuth: { token: string; refreshToken: string }
 }
 
-async function fetchAuthTokens(username: string, password: string) {
+export async function fetchAuthTokens(username: string, password: string, endpoint: string) {
+    const graphQLClient = new GraphQLClient(endpoint)
     return await graphQLClient.request<TAuthTokens>(mutation, {
         username: username,
         password: password
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,27 @@ import clear from "clear";
 import figlet from "figlet";
 import { program } from "commander";
 
+import { fetchAuthTokens } from "./auth"
 import { getAuthedWebsocket } from "./client";
 
 clear();
 console.log(
   chalk.red(figlet.textSync("sockbasher", { horizontalLayout: "full" }))
 );
+
+async function authHandler (env: any, options: any) {
+    const { user, password, host } = program.opts();
+    if (!host) program.help();
+    const auth_endpoint = `https://${host}/api/graphql`
+    const ws_endpoint = `ws://${host}/graphql`
+
+    try {
+        const auth = await fetchAuthTokens(user, password, auth_endpoint)
+        const ws = getAuthedWebsocket(ws_endpoint, auth.tokenAuth.token, console.log);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 program
   .description("bash the sock", {
@@ -25,13 +40,5 @@ program
   .option("-u, --user <user>", "User", "bill")
   .option("-p, --password <password>", "Password", "bill")
   .option("-t, --token <token>", "Auth token", "bla")
+  .action(authHandler)
   .parse(process.argv);
-
-const { token, endpoint } = program.opts();
-if (!endpoint) program.help();
-
-try {
-  const ws = getAuthedWebsocket(endpoint, token, console.log);
-} catch (e) {
-  console.log(e);
-}
