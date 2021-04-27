6fb3257b61f21dda46dd796cb552b86c049f0647
Oli Dacombe <olidacombe@gmail.com>
2021-04-27 14:05:49 +0100

    get some opts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
-import clear from 'clear';
-import figlet from 'figlet';
+import chalk from "chalk";
+import clear from "clear";
+import figlet from "figlet";
+import { program } from "commander";
 
 clear();
 console.log(
-    chalk.red(
-        figlet.textSync('sockbasher', {horizontalLayout: 'full'})
-    )
-)
+  chalk.red(figlet.textSync("sockbasher", { horizontalLayout: "full" }))
+);
 
+program
+  .arguments("<url>")
+  .description("bash the sock", {
+    url: "the sock to bash",
+  })
+  .option("-u, --user", "User")
+  .option("-p, --password", "Password")
+  .parse(process.argv);
+
+const { host, user, password } = program.opts();
+if (!host) program.help();
