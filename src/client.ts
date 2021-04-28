6f9b112c1873a480212ff97587db6f8be5399cab
Oli Dacombe <olidacombe@gmail.com>
2021-04-28 11:27:10 +0100

    start getting hash counts

--- src/client.ts
+++ src/client.ts
@@ -1,27 +1,45 @@
 import WebSocket from "ws";
 import chalk from "chalk";
+import crypto from "crypto";
 import subscriptions from "./subscriptions";
 
 class Client {
   private ws: WebSocket;
   private changeCallback: ((client: Client) => void) | null;
+  private messageRoster: { [name: string]: number };
 
   constructor(
     url: string,
     authToken: string,
     changeCallback?: (client: Client) => void
   ) {
+    this.messageRoster = {};
     this.changeCallback = changeCallback || null;
     this.ws = getAuthedWebsocket(url, authToken, this.onMessage, subscriptions);
   }
 
-  onMessage = (m: any) => {
+  onMessage = (m: string) => {
     console.log(chalk.yellow(m));
     try {
       const { type, ...data } = JSON.parse(m);
+      this.logMessage(m);
       if (type === "data" && this.changeCallback) this.changeCallback(this);
     } catch {}
   };
+
+  get hashCounts() {
+    return { ...this.messageRoster };
+  }
+
+  logMessage = (m: string) => {
+    const hash = crypto.createHash("sha1").update(m).digest("base64");
+    const count = (this.messageRoster[hash] || 0) + 1;
+    this.messageRoster[hash] = count;
+  };
+
+  toString() {
+    return JSON.stringify(this.messageRoster, null, 2);
+  }
 }
 
 const getAuthedWebsocket = (
