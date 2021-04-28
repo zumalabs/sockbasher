969f1925ac9e5558f3bfeb25d64136216128f086
Tyrone Cutajar <tj.cutajar@gmail.com>
2021-04-28 13:58:30 +0100

    Auth (#1)

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import clear from "clear";
 import figlet from "figlet";
 import { program } from "commander";
 
+import { fetchAuthTokens } from "./auth"
 import ClientHerd from "./clientHerd";
 
 clear();
@@ -12,31 +13,38 @@ console.log(
   chalk.red(figlet.textSync("sockbasher", { horizontalLayout: "full" }))
 );
 
+async function authHandler (env: any, options: any) {
+    const { user, password, token, host, num } = program.opts();
+    if (!host) program.help();
+    const secure = !host.includes("localhost")
+    const auth_endpoint = `${secure ? "https" : "http"}://${host}/api/graphql`
+    const ws_endpoint = `${secure ? "wss" : "ws"}://${host}/graphql`
+
+    try {
+        const authToken = await fetchAuthTokens(user, password, auth_endpoint, token)
+        const herd = new ClientHerd(
+            ws_endpoint,
+            authToken,
+            (herd) => console.log(chalk.magenta(herd)),
+            num
+        );
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 program
   .description("bash the sock", {
     url: "the sock to bash",
   })
   .option(
-    "-e, --endpoint <endpoint>",
-    "Endpoint",
-    "ws://localhost:3000/graphql"
+    "-e, --host <host>",
+    "Host",
+    "localhost:3000"
   )
-  .option("-n --num <num>", "Number of websocket connections", parseInt, 10)
+  .option("-n, --num <num>", "Number of websocket connections", parseInt, 10)
   .option("-u, --user <user>", "User", "bill")
   .option("-p, --password <password>", "Password", "bill")
-  .option("-t, --token <token>", "Auth token", "bla")
+  .option("-t, --token <token>", "Auth token")
+  .action(authHandler)
   .parse(process.argv);
-
-const { token, endpoint, num } = program.opts();
-if (!endpoint) program.help();
-
-try {
-  const herd = new ClientHerd(
-    endpoint,
-    token,
-    (herd) => console.log(chalk.magenta(herd)),
-    num
-  );
-} catch (e) {
-  console.log(e);
-}
